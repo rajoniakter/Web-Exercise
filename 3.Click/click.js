d1ebbcbe9d871743a6a041f341a1b51a04a5f586
rajoniakter <rajoniakter1709@gmail.com>
2021-12-28 22:22:39 +0600

    Click

---
+++ 3.Click/click.js
@@ -0,0 +1,18 @@
+
+
+    
+        const btn = document.getElementById("btn");
+
+        const btn2 = document.getElementById("btn");
+
+        btn.addEventListener("click", ()=>{
+           btn.innerHTML=`<h1>I am rajoni</h1>
+           <h2>shathi</h2>`
+
+        })
+        btn2.addEventListener("mousedown", ()=>{
+            alert("rajoni is a good girl")
+        })
+
+   
+    
